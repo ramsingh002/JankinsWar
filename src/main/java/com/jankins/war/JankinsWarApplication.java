f55e80d2a01492b65bef7789cc8e5756804caed4
DELL <ram.singh002@yahoo.com>
2025-04-30 12:04:45 +0530

    first commit

---
+++ src/main/java/com/jankins/war/JankinsWarApplication.java
@@ -0,0 +1,13 @@
+package com.jankins.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankinsWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankinsWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jankins/war/JankinsWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jankins.war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankinsWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
